fa95ca395cc5c9634a6dfe36f91141db5782ff26
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-09-21 13:59:24 -0400

    OK

--- src/game/read_map.c
+++ src/game/read_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smayrand <smayrand@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 17:08:27 by smayrand          #+#    #+#             */
-/*   Updated: 2023/09/20 13:42:35 by smayrand         ###   ########.fr       */
+/*   Updated: 2023/09/21 13:59:00 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,18 @@ void	extract_file(char **argv, t_game *game)
 	char	*temp;
 	int		fd;
 
-game->is_playing = 0; // temporary just to let te program compile MUST REMOVE!
+game->is_playing = 0; // temporary just to let the program compile MUST REMOVE!
 	fd = open(argv[0], O_RDONLY);
 	temp = get_next_line(fd);
 	while (temp != NULL)
 	{
 		printf("%s", temp);
 		temp = get_next_line(fd);
+		if (temp && ft_strnstr(temp, "NO", 2) != 0)
+		else if (temp && ft_strnstr(temp, "SO", 2) != 0)
+		else if (temp && ft_strnstr(temp, "EA", 2) != 0)
+		else if (temp && ft_strnstr(temp, "WE", 2) != 0)
+		
+			printf("This is NO texture : %s\n", temp);
 	}
 }
