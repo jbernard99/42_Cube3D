f77edcc1f532c95ae2acfac035f90f6ca2451c61
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-09-22 11:25:51 -0400

    OK

--- src/game/game.c
+++ src/game/game.c
@@ -6,29 +6,28 @@
 /*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 21:25:17 by jbernard          #+#    #+#             */
-/*   Updated: 2023/09/21 14:50:48 by jbernard         ###   ########.fr       */
+/*   Updated: 2023/09/22 11:24:40 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cube.h"
 
-int	**init_map(int map_h, int map_w)
+int **init_map(int map_h)
 {
-	int **x;
+	int	**m;
 	
-	x = ft_calloc(map_w, sizeof(int*));
-
-	return (x);
+	m = ft_calloc(map_h, sizeof(int*));
+	
+	return (m);
 }
 
 void add_line(t_game *game, char *temp, int y)
 {
 	int *line;
 
-	game->m->map[y] = ft_calloc(ft_strlen(temp), sizeof(int));
+	game->m.map[y] = ft_calloc(ft_strlen(temp), sizeof(int));
 	
 }
-
 
 t_game init_game(t_settings *s)
 {
@@ -38,7 +37,6 @@ t_game init_game(t_settings *s)
 	game.mlx = mlx_init(s->scrn_w, s->scrn_h, "Forza Horizon 6", false);
 	game.img = mlx_new_image(game.mlx, s->scrn_w, s->scrn_h);
 	game.s = s;
-	game.m = init_map()
 	game.is_playing = 0;
 	game.old_time = 0;
 	game.time = 0;
